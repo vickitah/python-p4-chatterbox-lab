5c3ad5b02a3a705b5da0b72dcb32d9b41f3ee2ef
Vickitah <victor.mutuma@student.moringaschool.com>
2025-06-18 08:25:25 +0300

    save completed changes

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
-
 from models import db, Message
 
 app = Flask(__name__)
@@ -11,16 +10,44 @@ app.json.compact = False
 
 CORS(app)
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([message.to_dict() for message in messages]), 200
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods=['POST'])
+def post_message():
+    data = request.get_json()
+    try:
+        message = Message(
+            body=data['body'],
+            username=data['username']
+        )
+        db.session.add(message)
+        db.session.commit()
+        return jsonify(message.to_dict()), 201
+    except Exception as e:
+        return {'error': str(e)}, 400
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get_or_404(id)
+    data = request.get_json()
+
+    if 'body' in data:
+        message.body = data['body']
+
+    db.session.commit()
+    return jsonify(message.to_dict()), 200
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return {}, 204
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/seed.py
+++ server/seed.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from random import choice as rc
-
 from faker import Faker
 
 from app import app
@@ -9,25 +8,20 @@ from models import db, Message
 
 fake = Faker()
 
-usernames = [fake.first_name() for i in range(4)]
+usernames = [fake.first_name() for _ in range(4)]
 if "Duane" not in usernames:
     usernames.append("Duane")
 
 def make_messages():
-
     Message.query.delete()
-    
-    messages = []
 
-    for i in range(20):
-        message = Message(
-            body=fake.sentence(),
-            username=rc(usernames),
-        )
-        messages.append(message)
+    messages = [
+        Message(body=fake.sentence(), username=rc(usernames))
+        for _ in range(20)
+    ]
 
     db.session.add_all(messages)
-    db.session.commit()        
+    db.session.commit()
 
 if __name__ == '__main__':
     with app.app_context():
